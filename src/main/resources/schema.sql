90d71a0f81d72e4b6ce3f744953f285a71c086fd
Nguyen Huu Quan <huuquan.nguyen.qt@gmail.com>
2021-12-19 19:29:03 +0700

    fix db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `testjavabackend`.`appuser` (
+CREATE TABLE IF NOT EXISTS `heroku_dc7874158b337e2`.`appuser` (
     `id` VARCHAR(8) NOT NULL,
     `username` VARCHAR(50) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS `testjavabackend`.`appuser` (
     PRIMARY KEY(`id`)
     );
 
-CREATE TABLE IF NOT EXISTS `testjavabackend`.`role`(
+CREATE TABLE IF NOT EXISTS `heroku_dc7874158b337e2`.`role`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL UNIQUE,
     PRIMARY KEY(`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `testjavabackend`.`user_role`(
+CREATE TABLE IF NOT EXISTS `heroku_dc7874158b337e2`.`user_role`(
     `user_id` VARCHAR(8) NOT NULL,
     `role_id` INT NOT NULL,
     FOREIGN KEY (`user_id`) REFERENCES `appuser`(`id`),
